0b361cada9908f36d0498687e89c322c840f9412
Fredrik Ellis <fredrik.ellis@enewit.se>
2024-01-13 22:15:15 +0100

    Update acr.tf

--- terraform/acr.tf
+++ terraform/acr.tf
@@ -10,23 +10,6 @@ resource "azurerm_resource_group" "rg_acr" {
   }
 }
 
-# Create ACR user assigned identity
-resource "azurerm_user_assigned_identity" "acr_identity" {
-  resource_group_name = azurerm_resource_group.rg_acr.name
-  location            = azurerm_resource_group.rg_acr.location
-  tags                = merge(local.default_tags, var.acr_tags)
-
-  name = "${var.acr_name}Identity"
-  depends_on = [
-    azurerm_resource_group.rg_acr,
-  ]
-  lifecycle {
-    ignore_changes = [
-      tags
-    ]
-  }
-}
-
 # Create the Container Registry
 resource "azurerm_container_registry" "acr" {
   name                  = lower("${var.acr_name}0${var.environment}")
@@ -35,12 +18,6 @@ resource "azurerm_container_registry" "acr" {
   sku                   = var.acr_sku
   admin_enabled         = var.acr_admin_enabled
   data_endpoint_enabled = var.data_endpoint_enabled
-  identity {
-    type = "UserAssigned"
-    identity_ids = [
-      azurerm_user_assigned_identity.acr_identity.id
-    ]
-  }
   tags = merge(local.default_tags, var.acr_tags)
   lifecycle {
     ignore_changes = [
